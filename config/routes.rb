d744e1813eb2e7c6329d3c23d7433124a3b77c3a
yuri0818 <yyyy.0818.0830@gmail.com>
2020-09-10 22:27:46 +0900

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
 end
