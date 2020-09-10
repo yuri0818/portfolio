c93ecb951447d87d4dec46b670e887f163159085
yuri0818 <yyyy.0818.0830@gmail.com>
2020-09-10 23:49:42 +0900

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end 
+  
   def new
+    @user = User.new 
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
